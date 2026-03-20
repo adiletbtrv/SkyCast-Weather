8dc238720bf1e8917a5cf9f95eccaa92e3d67b31
Adilet Batyrov <adiletbatyrov2009@gmail.com>
2026-03-21 03:52:25 +0600

    Gh pages hotfix

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,21 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      base: '/SkyCast-Weather',
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+export default defineConfig({
+  plugins: [react()],
+  base: '/SkyCast-Weather/',
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  },
 });
